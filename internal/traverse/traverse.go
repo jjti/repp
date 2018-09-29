36caf1ee59fd7b4637ea44f4f058e09705722ad9
JJTimmons <joshua.timmons1@gmail.com>
2018-09-29 19:12:45 -0400

    making and moving to a private node struct type in traverse.go
    Former-commit-id: e9b6a8efba34b4b4035ae0ada7e869b3802f4fa1

--- internal/traverse/traverse.go
+++ internal/traverse/traverse.go
@@ -7,48 +7,86 @@ import (
 	"math"
 	"sort"
 
+	"github.com/jjtimmons/decvec/config"
 	"github.com/jjtimmons/decvec/internal/frag"
 )
 
-// Traverse the matches on the target fragment
-// and build up a list of possible assemblies
-func Traverse(f *frag.Fragment) [][]frag.Fragment {
-	var assemblies [][]frag.Fragment
+// node is a single node within the DP tree for building up
+// the vector from smaller building fragments
+type node struct {
+	// start of this node on the target vector (which has been 3x'ed for BLAST)
+	start int
 
-	// get each fragments minimum number of fragments in an assembly
-	// dists := calcFragDistance(f)
-
-	return assemblies
+	// end of this node on the target vector
+	end int
 }
 
-// calcFragDistance is for calculating each fragment's distance from
-// a vector "endpoint"
+// Traverse the matches on the target fragment
+// and build up a list of possible assemblies
+func Traverse(f *frag.Fragment) {
+	c := config.NewConfig()
+	firstBP := len(f.Seq)
+
+	// map fragment Matches to nodes
+	var nodes []node
+	for _, m := range f.Matches {
+		nodes = append(nodes, node{
+			start: m.Start,
+			end:   m.End,
+		})
+	}
+
+	// get each fragments minimum number of fragments in an assembly
+	dists := distanceToEnd(nodes, len(f.Seq), c.Synthesis.MaxLength)
+
+	// remove all fragments that are greater than the target distance
+	var filteredNodes []node
+	for m, dist := range dists {
+		// if it overlaps with the start of the search range
+		// 	compare it against the max-distance from the end
+		// else
+		// 	check if it's max-distance - 1 from end and remove
+		// 	it if it's not
+		if m.start < firstBP {
+			if dist < c.Fragments.MaxCount {
+				filteredNodes = append(filteredNodes, m)
+			}
+		} else {
+			if dist+1 < c.Fragments.MaxCount {
+				filteredNodes = append(filteredNodes, m)
+			}
+		}
+	}
+}
+
+// distanceToEnd is for calculating each fragment's minimum distance to
+// a vector "endpoint". used to weed out fragments that are too far
+// away from the end of the target
 //
 // for each building fragment, find the minimum number of fragments
 // needed to get from it to a "last-bp" (2x the length of the target
 // fragment's sequence length)
 //
 // maxSynth is the maximum length of a synthesized stretch of DNA
-func calcFragDistance(target *frag.Fragment, maxSynth int) map[frag.Match]int {
+func distanceToEnd(nodes []node, seqL int, maxSynth int) map[node]int {
 	// last bp within range being scanned
-	lastBP := 2 * len(target.Seq)
+	lastBP := 2 * seqL
 	// map from a fragment to the minimum number of fragments
 	// needed in an assembly including it
-	dists := make(map[frag.Match]int)
+	dists := make(map[node]int)
 
 	// sort by start
-	sort.Slice(target.Matches, func(i, j int) bool {
-		return target.Matches[i].Start < target.Matches[j].Start
+	sort.Slice(nodes, func(i, j int) bool {
+		return nodes[i].start < nodes[j].start
 	})
 
 	// add a "sink" to ensure there's a building fragment just past
 	// the end of the scanned range
-	sink := frag.Match{
-		ID:    "sink",
-		Start: lastBP + 1,
-		End:   lastBP + 1,
+	sink := node{
+		start: lastBP + 1,
+		end:   lastBP + 1,
 	}
-	matches := append(target.Matches, sink)
+	nodes = append(nodes, sink)
 
 	// number of fragments distance for this fragment
 	// through to the target bp (2x the seqLength)
@@ -60,13 +98,13 @@ func calcFragDistance(target *frag.Fragment, maxSynth int) map[frag.Match]int {
 	// else
 	//	return the min-distance from this to all fragments
 	// 	this could reasonably be assembled with
-	var distFor func(int, frag.Match) int
-	distFor = func(i int, f frag.Match) int {
+	var distFor func(int, node) int
+	distFor = func(i int, f node) int {
 		if dist, cached := dists[f]; cached {
 			return dist
 		}
 
-		if f.End >= lastBP {
+		if f.end >= lastBP {
 			dists[f] = 1
 			return 1
 		}
@@ -74,9 +112,9 @@ func calcFragDistance(target *frag.Fragment, maxSynth int) map[frag.Match]int {
 		// calculate how many synthesis fragments are necessary to get
 		// from this fragment to each fragment after it
 		var synthCount []int
-		for _, m := range matches[i+1:] {
+		for _, n := range nodes[i+1:] {
 			synthsToNext := 0
-			distToNext := m.Start - f.End
+			distToNext := n.start - f.end
 			if distToNext > 0 {
 				synthsToNext = int(math.Ceil(float64(distToNext) / float64(maxSynth)))
 			}
@@ -85,8 +123,8 @@ func calcFragDistance(target *frag.Fragment, maxSynth int) map[frag.Match]int {
 
 		// find the minimum distance among these options, accounting
 		// for the fact that we need to each synthesis is an additional (unseen) fragment
-		minDistNext := 1 + synthCount[0] + distFor(i+1, matches[i+1])
-		for j, m := range matches[i+1:] {
+		minDistNext := 1 + synthCount[0] + distFor(i+1, nodes[i+1])
+		for j, m := range nodes[i+1:] {
 			distToFrag := 1 + synthCount[j] + distFor(i+j, m)
 			if distToFrag < minDistNext {
 				minDistNext = distToFrag
@@ -99,7 +137,7 @@ func calcFragDistance(target *frag.Fragment, maxSynth int) map[frag.Match]int {
 	}
 
 	// fill cache
-	distFor(0, matches[0])
+	distFor(0, nodes[0])
 
 	// delete the sink fragment
 	delete(dists, sink)
--- internal/traverse/traverse_test.go
+++ internal/traverse/traverse_test.go
@@ -3,8 +3,6 @@ package traverse
 import (
 	"fmt"
 	"testing"
-
-	"github.com/jjtimmons/decvec/internal/frag"
 )
 
 // test the ability to calculate distances from each fragment to the end
@@ -13,40 +11,33 @@ import (
 // we want to know how far each fragment is the from the "end of the vector",
 // so, looking at each fragment from the start, we can find which will be
 // included in assemblies with less than a certain number of fragments
-func Test_CalcFragDistance(t *testing.T) {
-	f1 := frag.Match{
-		ID:    "1",
-		Start: 8,
-		End:   13,
+func Test_MinDistanceToEnd(t *testing.T) {
+	f1 := node{
+		start: 8,
+		end:   13,
 	}
-	f2 := frag.Match{
-		ID:    "2",
-		Start: 12,
-		End:   15,
+	f2 := node{
+		start: 12,
+		end:   17,
 	}
-	f3 := frag.Match{
-		ID:    "3",
-		Start: 13,
-		End:   18,
+	f3 := node{
+		start: 13,
+		end:   18,
 	}
-	f4 := frag.Match{
-		ID:    "4",
-		Start: 15,
-		End:   20,
+	f4 := node{
+		start: 15,
+		end:   20,
 	}
-	f5 := frag.Match{
-		ID:    "5",
-		Start: 17,
-		End:   21,
+	f5 := node{
+		start: 17,
+		end:   21,
 	}
-	f := frag.Fragment{
-		Seq:     "atgatctagc", // 10 bp, search range will be seq[10:21]
-		Matches: []frag.Match{f1, f2, f3, f4, f5},
-	}
+	nodes := []node{f1, f2, f3, f4, f5}
 
-	dists := calcFragDistance(&f, 3)
+	// using a target vector sequence length of 10
+	dists := distanceToEnd(nodes, 10, 3)
 
-	// should only be 5 keys
+	// should only be 5 keys (one for each fragment)
 	if len(dists) != 5 {
 		t.Errorf("failed, distance map has %d keys, should have 5", len(dists))
 	}
@@ -55,11 +46,14 @@ func Test_CalcFragDistance(t *testing.T) {
 
 	// make sure the distance in the distance map is only as long as
 	// we expect it to be
-	checkDist := func(key frag.Match, dist int) {
+	checkDist := func(key node, dist int) {
 		if d, _ := dists[key]; d != dist {
-			t.Errorf("failed, distance map's value for %s is %d, not %d", key.ID, d, dist)
+			t.Errorf("failed, distance map's value for %v is %d, not %d", key, d, dist)
 		}
 	}
 	checkDist(f1, 3)
+	checkDist(f2, 2)
+	checkDist(f3, 2)
+	checkDist(f4, 1)
 	checkDist(f5, 1)
 }
