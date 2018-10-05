815bcf6aaf926431aadb3d8bb4739739af0d85e8
JJTimmons <joshua.timmons1@gmail.com>
2018-10-04 23:36:05 -0400

    start of parsing primer3 output, half finished

--- internal/frag/pcr_test.go
+++ internal/frag/pcr_test.go
@@ -17,5 +17,18 @@ func Test_setPrimers(t *testing.T) {
 
 	if p.Primers == nil || len(p.Primers) < 2 {
 		t.Error("failed to set primers on fragment")
+		return
+	}
+
+	pr := p.Primers[0]
+
+	// check that we got a sequence for the primer from the output file
+	if pr.seq == "" {
+		t.Errorf("failed to get a primer sequence for %v", pr)
+	}
+
+	// check that penalties and pair penalties were set
+	if pr.Tm < 1 || pr.Penalty < 1 || pr.PairPenalty < 1 || pr.GC < 1 {
+		t.Errorf("failed to parse primer3 penalty scores into primer: %v", pr)
 	}
 }
