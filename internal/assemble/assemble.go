d4fa1b869b571dd3b360c7a9224f5ff54af92e5c
JJTimmons <joshua.timmons1@gmail.com>
2018-10-07 15:36:48 -0400

    start of moving to a DAG based approach
    Former-commit-id: 7cbaab333b9290a53d42188c4f01b46bba41f6cb

--- internal/assemble/assemble.go
+++ internal/assemble/assemble.go
@@ -12,9 +12,20 @@ var (
 	conf = config.NewConfig()
 )
 
-// Assemble the BLAST matches, as nodes, into an assembly
-// that spans the template/target sequence and has a minimum
-// total cost
+// Assemble the BLAST matches into assemblies that span the target
+// sequence
+//
+// First build up assemblies, creating all possible assemblies that are
+// beneath the upper-bound limit on the number of fragments
+//
+// Then find the pareto optimal solutions that minimize either the cost
+// of the assembly or the number of fragments (relative to the other
+// assembly plans)
+//
+// Then, for each pareto optimal solution, traverse the assembly and
+// "fill-in" the nodes. Create primers on the node if it's a PCR Fragment
+// or create a sequence to be synthesized if it's a synthetic fragment.
+// Error out and repeat the build stage if a node fails to be filled
 func Assemble(matches []frag.Match, seqL int, from int, to int) {
 	// map fragment Matches to nodes
 	// store whether they're entry or terminal nodes,
