577608731dc760456838004fb082c7e662774ada
JJTimmons <joshua.timmons1@gmail.com>
2018-10-07 16:45:56 -0400

    mocking out of build flow

--- internal/assemble/build.go
+++ internal/assemble/build.go
@@ -1,5 +1,10 @@
 package assemble
 
+import (
+	"math"
+	"sort"
+)
+
 // build is for building up an ASSEMBLIES_LIST
 //
 // ASSEMBLIES_LIST is a map from the number of fragments in an assembly to
@@ -9,7 +14,7 @@ package assemble
 //
 // number of additional nodes to consider for each node
 // (ie additional number of fragments to try synthing to)
-// synth_count = math.max(10, 0.05 * len(nodes))
+// synth_count = math.max(5, 0.05 * len(nodes))
 //
 // // traverse the nodes
 // foreach this.node (sorted in reverse order):
@@ -22,6 +27,29 @@ package assemble
 // create a map from the number of fragments in each assembly to a list with assemblies
 // 		containing that many assemblies
 func build(nodes []node) (assemblies map[int]assembly) {
+	// number of nodes to try to synthesize to from each node (in addition to those with
+	// natural overlap)
+	synthCount := int(math.Max(5.0, 0.05*float64(len(nodes))))
+
+	// sort in descending order of end index
+	sort.Slice(nodes, func(i, j int) bool {
+		return nodes[i].end > nodes[j].end
+	})
+
+	// for every node in the list of reverse sorted nodes
+	for i, n := range nodes {
+		// for every overlapping fragment + synthCount more
+		for _, r := range n.reach(nodes, i, synthCount) {
+			// for every assembly on the reachable fragment
+			for _, a := range r.assemblies {
+				// see if we can create a new one with this node included
+				if newA, made := a.add(n); made {
+					// and add to this node's list of assemblies
+					n.assemblies = append(n.assemblies, newA)
+				}
+			}
+		}
+	}
 
 	return assemblies
 }
