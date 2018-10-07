4d4936516ff70e39b59d35f0bea7df79a10f5532
JJTimmons <joshua.timmons1@gmail.com>
2018-10-06 21:05:12 -0400

    returning a boolean if a mismatch is found

--- internal/blast/mismatch.go
+++ internal/blast/mismatch.go
@@ -36,13 +36,15 @@ func isMismatch(match frag.Match) bool {
 //
 // The parent sequence is passed as the entry id as it exists in the
 // blast database
-func Mismatch(primer, parent string) (match frag.Match, err error) {
+func Mismatch(primer, parent string) (mismatch bool, match frag.Match, err error) {
 	// path to the blastdbcmd binary
 	blastcmd, _ := filepath.Abs(path.Join("..", "..", "vendor", "ncbi-blast-2.7.1+", "bin", "blastdbcmd"))
 	// path to the entry batch file to hold the parent entry accession
 	entry, _ := filepath.Abs(path.Join(blastDir, parent+".entry"))
 	// path to the output sequence file
 	entryOutput, _ := filepath.Abs(path.Join(blastDir, parent+".out"))
+	// path the query sequence input file
+	in, _ := filepath.Abs(path.Join(blastDir, primer+"-"+parent+".query"))
 	// path to the blastOutput file
 	out, _ := filepath.Abs(path.Join(blastDir, parent+".blast"))
 
@@ -50,7 +52,10 @@ func Mismatch(primer, parent string) (match frag.Match, err error) {
 	// this was a 2-day bug I couldn't resolve...
 	// I was using the "-entry" flag on exec.Command, but have since
 	// switched to the far simpler -entry_batch command that fixes the issue
-	ioutil.WriteFile(entry, []byte(parent), 0666)
+	err = ioutil.WriteFile(entry, []byte(parent), 0666)
+	if err != nil {
+		return false, match, fmt.Errorf("failed to write batch entry list: %v", err)
+	}
 
 	// make a blast command
 	queryCmd := exec.Command(
@@ -62,40 +67,48 @@ func Mismatch(primer, parent string) (match frag.Match, err error) {
 		"-outfmt", "%f", // fasta format
 	)
 	if output, err := queryCmd.CombinedOutput(); err != nil {
-		return match, fmt.Errorf("failed to run blastdbcmd %s, %v, %s", entry, err, string(output))
+		return false, match, fmt.Errorf("failed to run blastdbcmd %s, %v, %s", entry, err, string(output))
+	}
+
+	// create blast input file
+	inContent := fmt.Sprintf(">primer\n%s\n", primer)
+	err = ioutil.WriteFile(in, []byte(inContent), 0666)
+	if err != nil {
+		return false, match, fmt.Errorf("failed to write primer sequence to query FASTA file: %v", err)
 	}
 
 	// blast the query sequence against the parent sequence
 	b := blastExec{
+		in:      in,
 		out:     out,
 		subject: entryOutput,
 	}
 
 	// execute blast
 	if err = b.runAgainst(); err != nil {
-		return match, fmt.Errorf("failed to run blast against parent: %v", err)
+		return false, match, fmt.Errorf("failed to run blast against parent: %v", err)
 	}
 
 	// get the BLAST matches
 	matches, err := b.parse()
 	if err != nil {
-		return match, fmt.Errorf("failed to parse matches from %s: %v", out, err)
+		return false, match, fmt.Errorf("failed to parse matches from %s: %v", out, err)
 	}
 
 	// parse the results and check whether any are cause for concern (by Tm)
 	primerSeen := false
 	for _, m := range matches {
 		// one of the matches will, of course, be against the primer itself
-		// we don't want to double count it
+		// and we don't want to double count it
 		if !primerSeen && m.Seq == primer {
 			primerSeen = true
 			continue
 		}
 
 		if isMismatch(m) {
-			return m, nil
+			return true, m, nil
 		}
 	}
 
-	return match, nil
+	return false, match, nil
 }
