f0af087777185d031ffeee143855d4b943b6af0c
JJTimmons <joshua.timmons1@gmail.com>
2018-10-06 21:05:12 -0400

    returning a boolean if a mismatch is found
    Former-commit-id: 7c889d18973230adb9e6dfed8dd40dcf7f0629ef

--- internal/blast/mismatch_test.go
+++ internal/blast/mismatch_test.go
@@ -1,19 +1,28 @@
 package blast
 
 import (
+	"fmt"
 	"path"
 	"path/filepath"
 	"testing"
 )
 
+// test that a primer's off-target is caught and flagged
 func TestMismatch(t *testing.T) {
 	db, _ = filepath.Abs(path.Join("..", "..", "test", "blast", "db"))
-	_, err := Mismatch(
-		"CAAATTTTGTAATCCAGAGGTTGATTATCGATAAGCTTGATATCGAATTCATAACTTCGTATAGCTACATTATACGAAGTTATTCT",
-		"gnl|addgene|85039.2",
-	)
+	primer := "AGTATAGGATAGGTAGTCATTCTT"
+	parent := "gnl|addgene|107006"
+	mismatchFound, m, err := Mismatch(primer, parent)
 
 	if err != nil {
 		t.Error(err)
+		return
 	}
+
+	if !mismatchFound {
+		t.Error("no mismatch caught")
+		return
+	}
+
+	fmt.Printf("%+v", m)
 }
