40675416cef8795955eeb505537fced47ab1a57f
JJTimmons <joshua.timmons1@gmail.com>
2018-10-07 21:44:25 -0400

    small bug fixes

--- internal/assemble/assembly.go
+++ internal/assemble/assembly.go
@@ -3,44 +3,48 @@ package assemble
 // assembly is a slice of nodes ordered by the nodes
 // distance from the end of the target vector
 type assembly struct {
+	// nodes, ordered by distance from the "end" of the vector
+	nodes []node
+
 	// estimated cost of making this assembly
 	cost float32
 
-	// nodes, ordered by distance from the "end" of the vector
-	nodes []node
+	// total number of synthetic nodes that will be needed to make this
+	synths int
 }
 
 // add a node to the start of this assembly.
 // Update the cost of the assembly to include the link between the new first node and the one after it.
-// store the node's id in the list of node ids.
+// Store the node's id in the list of node ids.
 // Complete if a node has matched up onto itself across the zero-index.
 // TODO: incorporate cost estimate of the last node in an assembly
 func (a *assembly) add(n node) (newAssembly assembly, created, complete bool) {
-	// do not create a new assembly if the total number of nodes
-	// would be less than the upper limit
-	if a.len()+1 > conf.Fragments.MaxCount {
-		return newAssembly, false, false
+	// we've complete an assembly if the node being added is the same as the
+	// first one in this assembly
+	complete = n.uniqueID == a.nodes[len(a.nodes)-1].uniqueID
+	if complete {
+		return *a, true, complete
 	}
 
-	// we have not exceeded the fragment count limit, will be making new assembly
-	created = true
-
 	// add to list of nodes, update cost, and return
-	if a.len() > 0 {
-		// we've complete an assembly if the id of a node is the same
-		complete = n.uniqueID == a.nodes[a.len()-1].uniqueID
+	if len(a.nodes) > 0 {
+		// calc the number of synthesis fragment to get to next fragment
+		synths := a.nodes[0].synthDist(n)
+
+		// stay beneath upper limit
+		if a.len()+synths+1 > conf.Fragments.MaxCount {
+			return newAssembly, false, false
+		}
 
 		return assembly{
-			nodes: append([]node{n}, a.nodes...),
-			cost:  n.costTo(a.nodes[0]),
-		}, created, complete
+			nodes:  append([]node{n}, a.nodes...),
+			cost:   n.costTo(a.nodes[0]),
+			synths: a.synths + synths,
+		}, true, false
 	}
 
 	// create the start of this assembly, no other nodes
-	return assembly{
-		nodes: []node{n},
-		cost:  0,
-	}, created, false
+	return assembly{[]node{n}, 0, 0}, true, false
 }
 
 // contains returns if the id of the node has already been seen in this assembly
@@ -49,14 +53,14 @@ func (a *assembly) contains(n node) (isContained bool) {
 		// they're the same if they have the same id and start index
 		// id isn't enough by itself because there may be multiple with the same
 		// entry id in the BLAST db
-		if otherN.id == n.id && otherN.start == n.start {
+		if otherN.uniqueID == n.uniqueID {
 			return true
 		}
 	}
 	return false
 }
 
-// len returns len(nodes)
+// len returns len(nodes) + the synthesis fragment count
 func (a *assembly) len() int {
-	return len(a.nodes)
+	return len(a.nodes) + a.synths
 }
--- internal/assemble/node.go
+++ internal/assemble/node.go
@@ -29,10 +29,48 @@ type node struct {
 // will return a negative number if this node overlaps with the other.
 // will return a positive number if this node doesn't overlap with the other.
 func (n *node) distTo(other node) (bpDist int) {
-	return other.end - n.start
+	return n.start - other.end
 }
 
-// reachable returns a slice of nodes that overlap with, or are the first synth_count nodes
+// synthDist returns the number of synthesized fragments that would need to be created
+// between one node and another if the two were to be joined, with no existing
+// fragments/nodes in-between, in an assembly
+func (n *node) synthDist(other node) (synthCount int) {
+	dist := n.distTo(other)
+
+	if dist > -20 {
+		// can't be negative before the ceil
+		floatDist := math.Max(1.0, float64(dist))
+		// split up the distance between them by the max synthesized fragment size
+		return int(math.Ceil(floatDist / float64(conf.Synthesis.MaxLength)))
+	}
+
+	// if the dist is <-20, there's enough overlap already for PCR
+	return 0
+}
+
+// costTo calculates the $ amount needed to get from this fragment
+// to the other node passed, either by PCR or synthesis
+//
+// PCR is of course preferred if we can add homology between this and
+// the other fragment with PCR homology arms alone
+//
+// Otherwise we find the total synthesis distance between this and
+// the other fragmetn and divide that by the cost per bp of synthesized DNA
+func (n *node) costTo(other node) (cost float32) {
+	dist := n.distTo(other)
+
+	if dist < -20 {
+		// there's already overlap between this node and the one being tested
+		// and enough for exiting homology to be enough
+		return 60 * conf.PCR.BPCost
+	}
+
+	// we need to create a new synthetic fragment to get from this fragment to the next
+	return float32(dist) * conf.Synthesis.BPCost
+}
+
+// reach returns a slice of nodes that overlap with, or are the first synth_count nodes
 // away from this one within a slice of ordered nodes
 //
 // nodes are nodes sorted in descending order of start index
@@ -61,41 +99,4 @@ func (n *node) reach(nodes []node, i, synthCount int) (reachable []node) {
 		}
 	}
 	return reachable
-}
-
-// synthDist returns the number of synthesized fragments that would need to be created
-// between one node and another if the two were to be joined, with no existing
-// fragments/nodes in-between, in an assembly
-func (n *node) synthDist(other node) (synthCount int) {
-	dist := n.distTo(other)
-
-	if dist > 0 {
-		// split up the distance between them by the maximum synthesized
-		// fragment size
-		return int(math.Ceil(float64(dist) / float64(conf.Synthesis.MaxLength)))
-	}
-
-	// they already overlap
-	return 0
-}
-
-// costTo calculates the $ amount needed to get from this fragment
-// to the other node passed, either by PCR or synthesis
-//
-// PCR is of course preferred if we can add homology between this and
-// the other fragment with PCR homology arms alone
-//
-// Otherwise we find the total synthesis distance between this and
-// the other fragmetn and divide that by the cost per bp of synthesized DNA
-func (n *node) costTo(other node) (cost float32) {
-	dist := n.distTo(other)
-
-	if dist < -20 {
-		// there's already overlap between this node and the one being tested
-		// and enough for exiting homology to be enough
-		return 60 * conf.PCR.BPCost
-	}
-
-	// we need to create a new synthetic fragment to get from this fragment to the next
-	return float32(dist) * conf.Synthesis.BPCost
 }
