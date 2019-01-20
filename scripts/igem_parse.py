9ca1a95bd5c43d64d51fd4ff0ff5a21cf43580ce
JJTimmons <joshua.timmons1@gmail.com>
2019-01-20 13:27:15 -0500

    fix igem xml parser (just regex out data)
    Former-commit-id: d573acca77e1680a845d6b4c8577df373cc586b4

--- scripts/igem_parse.py
+++ scripts/igem_parse.py
@@ -1,57 +1,60 @@
-import io
 import os
 import re
-from xml.etree import ElementTree as ET
-
-import requests
-from lxml import etree, html
 
 # output directory for igem
-os.chdir(os.path.join("assets", "igem", "files"))
+os.chdir(os.path.join("assets", "igem"))
 
 
 def clean_file():
-    parsed = ""
-    with open("xml_parts.xml", "r") as part_file:
-        parsed = unicode(part_file.read(-1), errors="ignore")
+    with open("xml_parts.xml", "rb") as part_file:
+        parsed = part_file.read().decode("utf-8", errors="ignore")
 
-    parsed = re.sub(r"<field name=\"sequence_sha1\">.*</field>\n", "", parsed)
-
-    with open("xml_parts.parsed.xml", "w") as part_file:
-        part_file.write(parsed)
+        with open("xml_parts.parsed.xml", "w") as part_file:
+            part_file.write(parsed)
 
 
-# clean_file()
+clean_file()
 
 # split up into separate parts. each a tuple with (BBa_name, seq, circular: bool)
 def get_parts():
-    tree = ET.parse('xml_parts.parsed.xml')
-    root = tree.getroot()
-
-    print root
-
-    def invalid_row(row):
-        return row.find("ok") != 1 or row.find("discontinued") == 1
+    # create element tree object
+    parts_string = open("xml_parts.parsed.xml", "r").read()
 
     parts = []  # list of name, seq, circular tuples
-    for row in root.iterfind("row"):
-        if invalid_row(row):
+    for row in parts_string.split("<row>"):
+        if '"discontinued">1<' in row or '"sample_status">Not in stock' in row:
             continue
 
-        # relevant rows: deprecated, part_type, part_name, status == "Deleted"?, discontinued == 1, ok == 1, sequence
-        name = row.find("part_name").text
-        type = row.find("part_type").text
-        seq = row.find("sequence").text
-        parts.append((name, seq, "backbone" in type.lower()))
+        name = ""
+        type = ""
+        seq = ""
+
+        # get the name match
+        name_match = re.search(r"<field name=\"part_name\">(\w*)</field>", row)
+        if name_match:
+            name = name_match[1]
+
+        # get the part type
+        type_match = re.search(r"<field name=\"part_type\">(\w*)</field>", row)
+        if type_match:
+            type = type_match[1]
+
+        # get the seq
+        seq_match = re.search(r"<field name=\"sequence\">(\w*)</field>", row)
+        if seq_match:
+            seq = seq_match[1]
+
+        if name and type and seq and len(seq) > 10:
+            is_circular = "backbone" in type.lower() or "plasmid" in type.lower()
+            parts.append((name, seq, is_circular))
     return parts
 
 
-print get_parts()[:5]
-
-
 def write_parts(parts):
+    os.chdir(os.path.join("db"))
+
     # write to the local filesystem
-    with open("igem.fa", "w") as output_parts:
+    with open("igem", "w") as output_parts:
         for (name, seq, circular) in parts:
             if circular:
                 output_parts.write(">gnl|igem|" + name + " circular fwd\n" + seq + "\n")
@@ -59,36 +62,4 @@ def write_parts(parts):
                 output_parts.write(">gnl|igem|" + name + " linear fwd\n" + seq + "\n")
 
 
-def parse_to_db(parts_file):
-    combined_output = ""
-
-    for part in parts_file.split(">")[1:]:
-        lines = part.split("\n")
-
-        header = lines[0]
-
-        part_id = header.split(" ")[0]
-
-        circular = "backbone" in header.lower()
-
-        seq_lines = [s.strip() for s in lines[1:]]
-        seq = "".join(seq_lines)
-
-        if len(seq) < 20:
-            continue
-
-        if circular:
-            combined_output += ">gnl|igem|" + part_id + " circular fwd\n" + seq + "\n"
-        else:
-            combined_output += ">gnl|igem|" + part_id + " linear fwd\n" + seq + "\n"
-
-    os.chdir(os.path.join("..", "db"))
-
-    with open("db", "w") as igem_db:
-        igem_db.write(combined_output)
-
-
-# write_parts(get_parts()) # write to the fs, copying the text from the webpage
-
-# with open("igem.fa", "r") as igem_text:
-#     parse_to_db(igem_text.read(-1))
+write_parts(get_parts())
